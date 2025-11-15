a0ae8968ed242473ad49e4d30789ac061b95db91
Mohamed Aziz <0xxevilshadowsxx0@gmail.com>
2025-11-15 18:52:20 +0100

    4th commit

---
+++ src/Projet/Sprint.java
@@ -0,0 +1,53 @@
+package Projet;
+
+import java.time.LocalDate;
+
+import User.chefprojet;
+import java.util.List;
+
+public class Sprint {
+    protected String idSprint;
+    protected String objectif;
+    protected LocalDate dateDebut;
+    protected LocalDate dateFin;
+    protected SprintStatut statut;
+    protected Project projet;
+    protected chefprojet chefprojet;
+    protected List<Tache> taches;
+
+    public Sprint(String idSprint, String objectif, LocalDate dateDebut,
+                  LocalDate dateFin, SprintStatut statut, Project projet,
+                  chefprojet chefprojet, List<Tache> taches) {
+        this.idSprint = idSprint;
+        this.objectif = objectif;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+        this.projet = projet;
+        this.chefprojet = chefprojet;
+        this.taches = taches;
+    }
+
+    public double calculerAvancementSprint() {
+        int totalTaches = taches.size();
+        if (totalTaches == 0) return 0.0;
+
+        long tachesCompletees = taches.stream()
+                .filter(tache -> tache.getStatut() == TaskStatue.TERMINEE)
+                .count();
+
+        return (tachesCompletees / (double) totalTaches) * 100;
+    }
+
+    public void ajouterTache(Tache tache) {
+        taches.add(tache);
+    }
+
+    public void retirerTache(Tache tache) {
+        taches.remove(tache);
+    }
+
+    public void cloturerSprint() {
+        this.statut = SprintStatut.TERMINÉ;
+    }
+}
